8c179f69ea14e7163a84fadfa5b699427ed41b7f
Zameer Fouzan <zmrfzn@gmail.com>
2022-11-10 15:21:25 +0530

    setup log forwarding to GCP from winston

--- app/logger.js
+++ app/logger.js
@@ -1,6 +1,13 @@
 const { createLogger, transports, format } = require("winston");
 const { combine, timestamp,prettyPrint } = format;
 
+
+// Imports the Google Cloud client library for Winston
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+
+const loggingWinston = new LoggingWinston();
+
+
 const logger = createLogger({
   level: "info",
   format: combine(
@@ -12,6 +19,7 @@ const logger = createLogger({
     //new transports:
     new transports.File({ filename: 'logs/error.log', level: 'error' }),
     new transports.File({ filename: 'logs/app.log' }),
+    loggingWinston
   ],
   //...
 });
