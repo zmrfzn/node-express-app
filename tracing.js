0b903b526d4ea4c0eb1bcd23f86715447df134eb
Zameer Fouzan <zmrfzn@gmail.com>
2023-01-19 14:29:44 +0530

    otel cors config sucess; along with FE

--- tracing.js
+++ tracing.js
@@ -2,12 +2,19 @@
 const opentelemetry = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const api = require('@opentelemetry/api');
 
 const { PgInstrumentation } = require('@opentelemetry/instrumentation-pg');
 
 
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+const { B3Propagator, B3InjectEncoding } = require('@opentelemetry/propagator-b3');
 
+const {
+  CompositePropagator,
+  W3CBaggagePropagator,
+  W3CTraceContextPropagator,
+} = require("@opentelemetry/core");
 
 // const Resource = require("@opentelemetry/resources");
 // const SemanticResourceAttributes = require("@opentelemetry/semantic-conventions");
@@ -28,6 +35,16 @@ const newRelicExporter = new OTLPTraceExporter({
         'api-key' : 'f5644626eef13f26d27746c6e381555ef9f9NRAL'
     }
 });
+
+
+api.propagation.setGlobalPropagator(new CompositePropagator({
+  propagators:[
+    new W3CBaggagePropagator(),
+    new W3CTraceContextPropagator(),
+    new B3Propagator(),
+    new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER})
+  ]
+}))
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: newRelicExporter,
