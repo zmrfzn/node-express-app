cfb21fa3e08c6469db10ccd97dfc875a172e0bc6
Zameer Fouzan <zmrfzn@gmail.com>
2023-04-03 11:12:13 +0530

    enable console logging

--- app/logger.js
+++ app/logger.js
@@ -10,6 +10,7 @@ const logger = createLogger({
   //logger method...
   transports: [
     //new transports:
+    new transports.Console(),
     new transports.File({ filename: 'logs/error.log', level: 'error' }),
     new transports.File({ filename: 'logs/app.log' }),
   ],
