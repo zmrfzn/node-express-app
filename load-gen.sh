cc06c70eefb0c10f15d6cb2ca009d932d48ac4d1
Zameer Fouzan <zmrfzn@gmail.com>
2023-04-17 13:16:59 +0530

    updated load generator scripts

--- load-gen.sh
+++ load-gen.sh
@@ -3,4 +3,4 @@
 URL=http://fullstack-js-o11y.$_SANDBOX_ID.instruqt.io:8080/api/tutorials
 URL2=http://fullstack-js-o11y.$_SANDBOX_ID.instruqt.io:8080/api/tutorials/categories
 
-npx load-generator --workers 4 --pause 100 "$URL" "$URL2" -p
+npx load-generator --workers 4 --pause 5000 "$URL" "$URL2" -p
