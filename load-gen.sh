df26676db87ccce2b6a27e00dedde582049f4689
zameer fouzan <zfouzan@newrelic.com>
2023-08-09 22:04:59 +0530

    Update load-gen.sh

--- load-gen.sh
+++ load-gen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-URL=http://fullstack-js-o11y.$_SANDBOX_ID.instruqt.io:8080/api/tutorials
-URL2=http://fullstack-js-o11y.$_SANDBOX_ID.instruqt.io:8080/api/tutorials/categories
+URL=http://$HOSTNAME.$_SANDBOX_ID.instruqt.io:8080/api/tutorials
+URL2=http://$HOSTNAME.$_SANDBOX_ID.instruqt.io:8080/api/tutorials/categories
+URL3=http://$HOSTNAME.$_SANDBOX_ID.instruqt.io:8080/api/tutorials/published
 
-npx load-generator --workers 4 --pause 5000 "$URL" "$URL2" -p
+npx load-generator --workers 4 --pause 5000 "$URL" "$URL2" "$URL3" -p
